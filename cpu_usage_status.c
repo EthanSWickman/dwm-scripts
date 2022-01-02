cde596eb2de23b6f618d11448b5ba9f9137df88f
ewickman <esw@halfangle.com>
2022-01-01 18:40:56 -0800

    added compiled binary to gitignore

--- cpu_usage_status.c
+++ cpu_usage_status.c
@@ -6,9 +6,7 @@
 /* read /proc/stat over a certain amount of time and detect the change in idle time vs active time to get the CURRENT usage percentage */
 
 void SetCpuUsageString(char* cpuUsageString) {
-  FILE
-
-  char* cpuUsage[4];
+  char cpuUsage[4];
 
   strcpy(cpuUsageString, cpuUsage);
 }
